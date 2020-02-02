33ee6c95aafd33060978a537e47b609287862087
sasha walker <sashawalker31@gmail.com>
2020-02-02 14:23:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -53,7 +53,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchies_language => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
      programmer_hash[:dennis_ritchies_language][:languages]
