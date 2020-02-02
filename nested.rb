1654e6ac166cb9e9ec1463f0c677130ec73ec1d4
sasha walker <sashawalker31@gmail.com>
2020-02-02 14:15:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,7 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
-programmer_hash[:yukihiro_matsumoto][:known_for][:languages]=["LISP", "c"]
-    
+programmer_hash[:yukihiro_matsumoto]=[:known_for=> "Ruby", :languages=> ["LISP", "C"]]
 end
 
 def changing_alan
